f1df4aa5cf1ac4515dd4efb6104ace7f19488d54
Love Saini <sainilove910@gmail.com>
2026-03-07 18:07:55 +0530

    update server

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://uurja.in",
+      "https://uurja.in",
+      "https://www.uurja.in"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
@@ -55,7 +56,7 @@ app.use("/api/admin", adminRoutes);
 // Firebase Routes
 app.use("/api/firebase", firebaseRoutes);
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
