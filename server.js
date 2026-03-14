b2d9d085f1c6e242369039dbbf6312f444e238f5
digitalinapp511-art <digitalinapp511@gmail.com>
2026-03-14 17:03:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js"; 
+import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import orderRoutes from "./routes/order.routes.js";
 import bannerRoutes from "./routes/banner.routes.js";
-import adminRoutes from "./routes/admin.routes.js"; 
-import firebaseRoutes from "./routes/firebase.routes.js"; 
+import adminRoutes from "./routes/admin.routes.js";
+import firebaseRoutes from "./routes/firebase.routes.js";
 
 dotenv.config();
 
@@ -18,47 +18,35 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-      "https://uurja.in",
-      "https://www.uurja.in"
+    "https://uurja.in",
+    "https://www.uurja.in"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Connect Database
 connectDB();
-
-
 
 // Test Route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
-
-// Product Routes
+// Routes
 app.use("/api/products", productRoutes);
-
-// Cart Routes
 app.use("/api/cart", cartRoutes);
-
-// Order Routes
 app.use("/api/orders", orderRoutes);
-
-// Banner Routes
 app.use("/api/banners", bannerRoutes);
-
-// Admin Routes
 app.use("/api/admin", adminRoutes);
-
-// Firebase Routes
 app.use("/api/firebase", firebaseRoutes);
 
-
+// PORT
 const PORT = process.env.PORT || 5000;
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
